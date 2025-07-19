5d573fa9189570d54761a471b119235c3ecdb031
Dietschy Julien <dietschyjulien@gmail.com>
2025-07-19 23:10:04 +0200

    feat: create supabase table and link

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://kfmwstofkxeexzoghsft.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmbXdzdG9ma3hlZXh6b2doc2Z0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5NTYwMDUsImV4cCI6MjA2ODUzMjAwNX0.wAYdmS5oxPYfpdDjHzRUuvsJcICuaJwRapQU3t5qwyE'
+};
