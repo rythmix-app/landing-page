c3b2898b24118e7d7a8f2997eea44b0dc88fe403
Dietschy Julien <julien@spotlag.com>
2025-07-19 15:20:55 +0200

    feat: create component for inout

---
+++ src/app/components/input-bloc/input-bloc.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-bloc',
+  imports: [],
+  templateUrl: './input-bloc.html',
+  styleUrl: './input-bloc.scss'
+})
+export class InputBloc {
+
+}
