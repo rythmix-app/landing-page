adbad7fd4b76b6b0ae55f919b780313cef393139
Dietschy Julien <dietschyjulien@gmail.com>
2025-07-20 13:47:00 +0200

    fix: hotfixed neonConnectionString

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ import {neon} from "@neondatabase/serverless";
 
 export const environment = {
   production: false,
-  neonConnectionString: process.env['NETLIFY_DATABASE_URL'],
+  neonConnectionString: process.env['NETLIFY_DATABASE_URL']!,
 };
