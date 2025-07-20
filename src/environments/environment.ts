0f532b4f30b38592b5275d3dac1e2aead123a953
Dietschy Julien <dietschyjulien@gmail.com>
2025-07-20 14:21:20 +0200

    fix: hotfixed neonConnectionString

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ import {neon} from "@neondatabase/serverless";
 
 export const environment = {
   production: false,
-  neonConnectionString: process.env['NETLIFY_DATABASE_URL']!,
+  neonConnectionString: 'postgresql://neondb_owner:npg_lNsC7SgF3cfY@ep-noisy-heart-aemvdl99-pooler.c-2.us-east-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
 };
